6265bb5fa7ea58194e6224aa80013b2febc562e7
Christian <explosivfibres@hotmail.com>
2024-05-23 10:03:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json())
+
+app.listen(process.env.PORT, () => {
+    console.log(`server runs on ${process.env.PORT}`)
+});
+
+mongoose.connect(process.env.MONGO_URL)
+
+mongoose.Schema
+
+const userSchema = mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String
+});
+
+const User = mongoose.model('User', userSchema)
+
+// Create
+app.post('/users', async (req,res) => {
+
+    try{
+        const user = new User(req.body)
+        await user.save();
+        res.status(201).send(user)
+    }catch (error){
+        res.status(400).send(error);
+    }
+});
