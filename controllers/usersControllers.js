fc222d65300d4ba5476e7eba73fc0d0d067dfd02
Christian <explosivfibres@hotmail.com>
2024-05-24 12:09:27 +0200

    +

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../models/userSchema');
 
+// Create user
 exports.createUser = async (req, res) => {
   try {
     const user = new User(req.body);
